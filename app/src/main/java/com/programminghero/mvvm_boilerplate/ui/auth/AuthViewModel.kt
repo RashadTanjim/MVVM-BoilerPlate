44c38c73a512dabedc96396e56df21549e704d1d
Md Rashad Tanjim <rashad.tanjim@northsouth.edu>
2021-03-13 19:33:08 +0600

    Login System Working!

--- app/src/main/java/com/programminghero/mvvm_boilerplate/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/programminghero/mvvm_boilerplate/ui/auth/AuthViewModel.kt
@@ -1,10 +1,26 @@
 package com.programminghero.mvvm_boilerplate.ui.auth
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.programminghero.mvvm_boilerplate.network.Resource
 import com.programminghero.mvvm_boilerplate.repository.AuthRepository
+import com.programminghero.mvvm_boilerplate.responses.LoginResponse
+import kotlinx.coroutines.launch
 
 class AuthViewModel(
     private val repository: AuthRepository
-): ViewModel(){
+) : ViewModel() {
 
+    private val _loginResponse: MutableLiveData<Resource<LoginResponse>> = MutableLiveData()
+    val loginResponse: LiveData<Resource<LoginResponse>>
+        get() = _loginResponse
+
+    fun login(
+        email: String,
+        password: String
+    ) = viewModelScope.launch {
+        _loginResponse.value = repository.login(email, password)
+    }
 }
--- app/src/main/java/com/programminghero/mvvm_boilerplate/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/programminghero/mvvm_boilerplate/ui/auth/LoginFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import com.programminghero.mvvm_boilerplate.R
 import com.programminghero.mvvm_boilerplate.databinding.FragmentLoginBinding
 import com.programminghero.mvvm_boilerplate.network.AuthApi
+import com.programminghero.mvvm_boilerplate.network.Resource
 import com.programminghero.mvvm_boilerplate.repository.AuthRepository
 import com.programminghero.mvvm_boilerplate.ui.base.BaseFragment
 
@@ -17,6 +20,24 @@ class LoginFragment : BaseFragment<AuthViewModel, FragmentLoginBinding, AuthRepo
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        viewModel.loginResponse.observe(viewLifecycleOwner, Observer {
+            when (it) {
+                is Resource.Success -> {
+                    Toast.makeText(requireContext(), it.toString(), Toast.LENGTH_SHORT).show()
+                }
+                is Resource.Failure -> {
+                    Toast.makeText(requireContext(), it.toString(), Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+
+        binding.buttonLogin.setOnClickListener{
+            val email = binding.editTextTextEmailAddress.text.toString().trim()
+            val password = binding.editTextTextPassword.text.toString().trim()
+
+            //@TODO ADD input validations
+            viewModel.login(email, password)
+        }
 
     }
 
