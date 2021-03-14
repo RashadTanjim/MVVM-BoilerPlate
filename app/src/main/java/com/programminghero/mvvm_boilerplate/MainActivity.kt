cdd1b65476a9ded07f25d8a6cd801ae446ccf3cb
Md Rashad Tanjim <rashad.tanjim@northsouth.edu>
2021-03-15 03:47:00 +0600

    hit Login API, to authenticate user part done

--- app/src/main/java/com/programminghero/mvvm_boilerplate/MainActivity.kt
+++ app/src/main/java/com/programminghero/mvvm_boilerplate/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.asLiveData
 import com.programminghero.mvvm_boilerplate.data.UserPreferences
 import com.programminghero.mvvm_boilerplate.ui.auth.AuthActivity
+import com.programminghero.mvvm_boilerplate.ui.home.HomeActivity
+import com.programminghero.mvvm_boilerplate.ui.startNewActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,8 +19,10 @@ class MainActivity : AppCompatActivity() {
         val userPreferences = UserPreferences(this)
 
         userPreferences.authToken.asLiveData().observe(this, Observer {
-            Toast.makeText(this, "Token is Null", Toast.LENGTH_SHORT).show()
-            startActivity(Intent(this, AuthActivity::class.java))
+            val activity = if(it == null) AuthActivity::class.java else HomeActivity::class.java
+           startNewActivity(activity)
         })
+
+
     }
 }
--- app/src/main/java/com/programminghero/mvvm_boilerplate/data/repository/AuthRepository.kt
+++ app/src/main/java/com/programminghero/mvvm_boilerplate/data/repository/AuthRepository.kt
@@ -1,9 +1,11 @@
 package com.programminghero.mvvm_boilerplate.data.repository
 
+import com.programminghero.mvvm_boilerplate.data.UserPreferences
 import com.programminghero.mvvm_boilerplate.data.network.AuthApi
 
 class AuthRepository(
-    private val api: AuthApi
+    private val api: AuthApi,
+    private val preferences: UserPreferences
 ) : BaseRepository() {
 
     suspend fun login(
@@ -11,5 +13,9 @@ class AuthRepository(
         password: String
     ) = safeApiCall {
         api.login(email, password)
+    }
+
+    suspend fun saveAuthToken(token: String) {
+        preferences.saveAuthToken(token)
     }
 }
